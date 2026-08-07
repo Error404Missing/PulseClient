0fbaad2c72d4c8b51471b3ad3a7068c6fddd245f
Error404Missing <lukabbxx@gmail.com>
2026-08-07 20:24:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const adminUserSelectTrigger = document.getElementById('admin-user-select-trigge
 const adminUserSearch = document.getElementById('admin-modal-user-search');
 const adminUserOptionsList = document.getElementById('admin-modal-user-options-list');
 
-const ADMIN_DISCORD_IDS = ["1475396409246089367", "", "1158855771031867432","","1350538035342737441"];
+const ADMIN_DISCORD_IDS = ["1475396409246089367", "386205109069152257", "1158855771031867432","","1350538035342737441"];
 let currentUser = null;
 let adminLicenses = [];
 let allUserProfiles = [];
